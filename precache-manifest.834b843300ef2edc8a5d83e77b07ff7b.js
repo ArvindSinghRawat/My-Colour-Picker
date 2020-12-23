51028cc26ca60b1d82f5a9a7165fdc9179a1764a
GitHub <github-action@users.noreply.github.com>
2020-12-23 17:56:37 +0000

    deploy files

---
+++ precache-manifest.834b843300ef2edc8a5d83e77b07ff7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "345d96318c9249200daa31f7c731df92",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc5f66b91de573f054ce",
+    "url": "./static/css/2.3a221eca.chunk.css"
+  },
+  {
+    "revision": "cb5565cb9302bd3997ce",
+    "url": "./static/css/main.2ac8d3be.chunk.css"
+  },
+  {
+    "revision": "dc5f66b91de573f054ce",
+    "url": "./static/js/2.f6b533d4.chunk.js"
+  },
+  {
+    "revision": "2e54b551fdc3988dd651d638460fe789",
+    "url": "./static/js/2.f6b533d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb5565cb9302bd3997ce",
+    "url": "./static/js/main.13ca5263.chunk.js"
+  },
+  {
+    "revision": "cdf412740fdc2204235c",
+    "url": "./static/js/runtime-main.e530ba34.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
